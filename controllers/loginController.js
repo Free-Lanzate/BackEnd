b476ea52cbb8ce599af7612949c295a903212d76
MARIA PAULA <macalderonj@unal.edu.co>
2022-05-22 16:38:59 -0500

    Login con tokens definitivo eso espero

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,11 +2,13 @@ const db = require("../models");
 //const connection = require("express");
 const User = db.User;
 const { v4: uuidv4 } = require('uuid');
+const Op = db.Sequelize.Op;
 
 exports.login = async (req, res) => {
     // Capture the input fields
     let username = req.body.username;
     let password = req.body.password;
+
     // Ensure the input fields exists and are not empty
     if (username && password) {
         // Execute SQL query that'll select the account from the database based on the specified username and password
@@ -14,35 +16,23 @@ exports.login = async (req, res) => {
         if (search === null) {
             res.send('Incorrect Username and/or Password!');
         } else {
-            // Authenticate the user
-            //req.session.loggedin = true;
-            //req.session.username = username;
-            // Redirect to home page
-            // res.redirect('/home');
-
-            res.send(generateToken(username));
+            let token;
+            User.findOne({ where: {username : username} })
+                .then(user => {
+                    token = generateToken(user);
+                    json = {"token": token}
+                    res.contentType('application/json');
+                    res.send(JSON.stringify(json));
+                    res.end();
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message:
+                            err.message || "Some error occurred while retrieving users."
+                    });
+                });
         }
-        /*connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-            // If there is an issue with the query, output the error
-            if (error) throw error;
-            // If the account exists
-            if (results.length > 0) {
-                // Authenticate the user
-                req.session.loggedin = true;
-                req.session.username = username;
-                // Redirect to home page
-                // res.redirect('/home');
-                res.send('Ok!');
-            } else {
-                res.send('Incorrect Username and/or Password!');
-            }
-            res.end();
-        });*/
-        res.end();
-    } else {
-        res.send('Please enter Username and Password!');
-        res.end();
-    };
+    }
 };
 
 exports.home = (req, res) => {
@@ -60,9 +50,9 @@ exports.home = (req, res) => {
 const secretKey = uuidv4();
 const nJwt = require('njwt');
 
-function generateToken(username){
+function generateToken(user){
     const claims = {
-        sub: username,
+        sub: user,
         iss: "https://freelanzate.com"
     };
 
@@ -70,3 +60,4 @@ function generateToken(username){
 
     return jwt.compact();
 }
+
